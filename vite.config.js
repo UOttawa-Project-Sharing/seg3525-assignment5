c4fde3a49785cb505fa78e9e8b04ccabd9bdd33a
Tristan Robichaud <contact@toxoscorp.com>
2025-07-16 08:15:23 -0400

    init project

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/seg3525-assignment5/',
+})
